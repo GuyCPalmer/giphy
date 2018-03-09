f36a4d3e042d1d5c4125c046ccbc9935fb7536a6
Guy Palmer <guypalmer@Guys-Air.lan>
2018-03-09 14:04:14 -0600

    update

--- app.js
+++ app.js
@@ -24,38 +24,45 @@ var topics = ["Bo Jackson", "Brett Favre", "Joe Montana", "Michael Jordan", "Rog
             for (var i = 0; i < results.length; i++) {
             if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
-                // creating paragraph tag with the results ratings
-                var p = $("<p>").text("rating: " + results[i].rating);
+                // creating div with the class Athletes pictures
+                var gifDiv = $("<div clas='athletePicture'>");
 
-                // creating and storing an image tag
+                //storing the results rating
+                var rating = results[i].rating;
+
+                //creating a p tag with the results 
+                var p = $('<p>').text("Rating: " + rating);
+
+                //creating an image tag
                 var athleteImage = $("<img>");
-                athleteImage.addClass("gify");
 
+                //image src tag to make it dance
                 athleteImage.attr("src", results[i].images.fixed_height_still.url);
                 athleteImage.attr("data-still", results[i].images.fixed_height_still.url);
                 athleteImage.attr("data-animate", results[i].images.fixed_height.url);
-                athleteImage.attr("data-state", "still")
+              
+                //appending the things to gifDiv
+                gifDiv.append(p);
+                gifDiv.append(athleteImage);
 
-                $("#athletes").append(athleteImage).append(p);
-             }
-            }
-        });
-    };
-
-    $(document).on("click", '.giphy', function() {
-        console.log(state)
+                $("gifsView").prepend(gifDiv);
+             athleteImage.on("click", function() {
+    
     // set the value of attribute
     var state = $(this).attr("data-state");
     // if clicked image is still, update to animate
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
+        console.log('still');
     }   else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
+        console.log('animate');
     }
     });
-
+ };
+};
 
     function renderButtons () {
         console.log(topics);
